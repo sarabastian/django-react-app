6546c1bf8fe967f9efd006c65f8c5aec78b7bd57
sarabastian <seb118@georgetown.edu>
2021-06-14 14:18:17 -0400

    admin interface CRUD

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_nsi63v*1$sml47@)r)avi1ni2s39=o9m3q=hbpq4^w0yi0*c='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
