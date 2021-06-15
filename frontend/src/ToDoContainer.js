75722276538a938b6dc7aeb3f651c523f8c89604
sarabastian <seb118@georgetown.edu>
2021-06-15 15:14:40 -0400

    add task

--- frontend/src/ToDoContainer.js
+++ frontend/src/ToDoContainer.js
@@ -1,5 +1,14 @@
 import React, { useEffect } from "react";
 import ToDoCard from "./ToDoCard";
+import {
+  Modal,
+  ModalHeader,
+  Textarea,
+  ModalFooter,
+  Button,
+  Input,
+  Label,
+} from "@windmill/react-ui";
 
 const ToDoContainer = () => {
   useEffect(() => {
@@ -35,7 +44,7 @@ const ToDoContainer = () => {
         "Content-Type": "application/json",
         "X-CSRFToken": csrftoken,
       },
-    }).then(refreshList());
+    }).then((r) => refreshList());
   };
 
   const refreshList = () => {
@@ -44,17 +53,83 @@ const ToDoContainer = () => {
       .then((t) => setToDos(t));
   };
 
-  //   const addToDo = () => {
-  //     fetch("http://localhost:2000/api/todos/", {
-  //         method: 'POST',
-  //         body:
-  //     })
-  //   }
+  const addTask = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:2000/api/todos/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+      body: JSON.stringify({
+        title: newTaskTitle,
+        description: newTaskDesc,
+        completed: false,
+      }),
+    })
+      .then((r) => r.json())
+      .then((r) => refreshList());
+  };
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const [newTaskTitle, setNewTaskTitle] = React.useState("");
+  const [newTaskDesc, setNewTaskDesc] = React.useState("");
+  console.log(newTaskDesc);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleTitle = (e) => {
+    setNewTaskTitle(e.target.value);
+  };
+  const handleDescription = (e) => {
+    setNewTaskDesc(e.target.value);
+  };
+
   return (
     <div className="mt-8 grid grid-cols-2 gap-4">
       {toDos.map((t) => (
         <ToDoCard toDo={t} key={t.id} handleDelete={handleDelete} />
       ))}
+      <div>
+        <Button onClick={openModal}>Open modal</Button>
+      </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <ModalHeader>New Task</ModalHeader>
+        <form onSubmit={addTask}>
+          <Label>
+            <span className="ml-2">Title</span>
+            <Input onChange={handleTitle} type="text"></Input>
+          </Label>
+          <Label>
+            <span className="ml-2"></span>
+            <Textarea
+              onChange={handleDescription}
+              className="mt-1"
+              rows="3"
+              placeholder="More about this task."
+              type="text"
+            />
+          </Label>
+
+          <ModalFooter>
+            <Button
+              className="w-full sm:w-auto"
+              layout="outline"
+              onClick={closeModal}
+            >
+              Cancel
+            </Button>
+            <button onClick={closeModal} className="w-full sm:w-auto">
+              Submit
+            </button>
+          </ModalFooter>
+        </form>
+      </Modal>
     </div>
   );
 };
