6f61999f0be501dc38a3931df1e9a4dc28bbc8c8
sarabastian <seb118@georgetown.edu>
2021-06-15 13:40:31 -0400

    need trailing slash in fetch address for delete

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,6 +128,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+
 ]
--- frontend/src/ToDoContainer.js
+++ frontend/src/ToDoContainer.js
@@ -9,13 +9,33 @@ const ToDoContainer = () => {
   }, []);
 
   const [toDos, setToDos] = React.useState([]);
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  var csrftoken = getCookie("csrftoken");
 
   const handleDelete = (id) => {
-    fetch(`http://localhost:2000/api/todos/${id}`, {
+    fetch(`http://localhost:2000/api/todos/${id}/`, {
       method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then(refreshList());
+
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken,
+      },
+    }).then(refreshList());
   };
 
   const refreshList = () => {
@@ -23,6 +43,13 @@ const ToDoContainer = () => {
       .then((r) => r.json())
       .then((t) => setToDos(t));
   };
+
+  //   const addToDo = () => {
+  //     fetch("http://localhost:2000/api/todos/", {
+  //         method: 'POST',
+  //         body:
+  //     })
+  //   }
   return (
     <div className="mt-8 grid grid-cols-2 gap-4">
       {toDos.map((t) => (
